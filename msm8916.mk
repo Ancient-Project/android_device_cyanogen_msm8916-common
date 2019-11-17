AndroiableDroid/device_LYF_msm8916-common

msm8916-common: Update Some for Treble
Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

msm8916.mk
@@ -111,11 +111,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 PRODUCT_PACKAGES += \
-    libshims_ims \
-    libshim_boringssl \
-    libshims_camera
-
-PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
 
@@ -146,6 +141,10 @@ endif
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
 
 # Keymaster
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
@@ -289,6 +288,28 @@ PRODUCT_COPY_FILES += \
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.cyanogen_8916
+
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    mobee01a-vndk
+
+# Vendor Specific
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@1.0.vendor \
+    android.hardware.camera.common@1.0.vendor \
+
+# Sensor
+PRODUCT_PACKAGES += \
+   android.hardware.sensors@1.0.vendor
+
+# Display
+PRODUCT_PACKAGES += \
+   libhardware_legacy.vendor \
+   libbinder.vendor \
+   libui.vendor \
+   android.hardware.configstore@1.0.vendor \
+   android.hardware.configstore-utils.vendor \
+   libstagefright_foundation.vendor
 
 # inherit from the proprietary version
 $(call inherit-product, vendor/LYF/msm8916-common/msm8916-common-vendor.mk)

Ancient-Project/android_device_cyanogen_msm8916-common

msm8916.mk
@@ -115,6 +115,10 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # Keymaster
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
 PRODUCT_PACKAGES += \
@@ -225,3 +229,29 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.cyanogen_8916
+
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    ido-vndk
+
+# Vendor Specific
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@1.0.vendor \
+    android.hardware.camera.common@1.0.vendor \
+
+# Sensor
+PRODUCT_PACKAGES += \
+   android.hardware.sensors@1.0.vendor
+
+# Display
+PRODUCT_PACKAGES += \
+   libhardware_legacy.vendor \
+   libbinder.vendor \
+   libui.vendor \
+   android.hardware.configstore@1.0.vendor \
+   android.hardware.configstore-utils.vendor \
+   libstagefright_foundation.vendor