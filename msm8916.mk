5ea8fc2340898ee2c65b4de09be23b4c007a170e
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-11-17 18:13:57 +0700

    msm8916-common: move to vendor_prop.mk

--- msm8916.mk
+++ msm8916.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
